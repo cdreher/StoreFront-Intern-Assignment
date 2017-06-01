5f7c05124652aad22bc6740420e38290cccc47ce
Collin Dreher <collin.dreher@gmail.com>
2017-06-01 10:11:04 -0400

    dsfsd

---
+++ Week 3 Intern Assignment/Models/CustomerBaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week_3_Intern_Assignment.Models
+{
+    public class CustomerBaseViewModel
+    {
+        public string UserName { get; set; }
+        public int UserID { get; set; }
+
+    }
+}
