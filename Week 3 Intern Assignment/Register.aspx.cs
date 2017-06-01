7481643ef95b1a116914d8731df1885a60f4a757
Collin Dreher <collin.dreher@gmail.com>
2017-06-01 15:15:08 -0400

    make register and login page

---
+++ Week 3 Intern Assignment/Register.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Week_3_Intern_Assignment
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
