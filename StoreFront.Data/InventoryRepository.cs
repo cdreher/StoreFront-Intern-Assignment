63662efcb4e737e96fe14746072bf0a9c5b45fa4
Collin Dreher <collin.dreher@gmail.com>
2017-06-26 11:08:30 -0400

    week 5 assignment skeleton started

---
+++ StoreFront.Data/InventoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreFront.Data
+{
+    public class InventoryRepository
+    {
+        public List<Product_table> SearchProducts(String searchText)    //return a list of "Product", that have name or description containing "searchText"
+        {
+            return null;
+        }
+
+        public Product_table GetProduct(int id)     //return an instance of "Product", which will have all the fields in the "Product" class. Populated inside the method & returned.
+        {
+            return null;
+        }
+    }
+}
---
+++ StoreFront.Data/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreFront.Data
+{
+    public class OrderRepository
+    {
+        public Order_table GetOrder(int id)     //return an instance of "Order", which is the row in the table w/ the corresponding id
+        {
+            return null;
+        }
+    }
+}
