e6f2118b44bdaeada3f04ca8fdb7d29694494c00
Collin Dreher <collin.dreher@gmail.com>
2017-06-26 16:20:32 -0400

    place order button opens empty page

---
+++ Week 3 Intern Assignment/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreFront.Data;
+
+namespace Week_3_Intern_Assignment.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PlaceOrder()
+        {
+            return View();
+        }
+    }
+}
