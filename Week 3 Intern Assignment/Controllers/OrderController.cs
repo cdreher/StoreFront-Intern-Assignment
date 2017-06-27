db33fb821250649cd52b72ccbe382e5f556ffd62
Collin Dreher <collin.dreher@gmail.com>
2017-06-27 16:16:52 -0400

    place order dropdown almost completed

--- Week 3 Intern Assignment/Controllers/OrderController.cs
+++ Week 3 Intern Assignment/Controllers/OrderController.cs
@@ -9,14 +9,42 @@ namespace Week_3_Intern_Assignment.Controllers
 {
     public class OrderController : Controller
     {
+        StoreFrontEntities db = new StoreFrontEntities();
         // GET: Order
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult PlaceOrder()
+        public ActionResult Address()
         {
+            var user = db.User_table.Where(a => a.UserName == HttpContext.User.Identity.Name).FirstOrDefault();
+            var addresses = db.Address_table.Where(a => a.UserID == user.UserID);
+            List<SelectListItem> li = new SelectList(addresses, "AddressID", "Address1").ToList();
+            li.Add(new SelectListItem { Text = "Enter new address" });
+            ViewBag.listAddresses = li;
+
+            var states = db.State_table.ToList();
+            ViewBag.listStates = new SelectList(states, "StateID", "StateName");
             return View();
         }
+
+        [HttpPost]
+        public ActionResult AddNewAddress(Address_table jawn)
+        {
+            var user = db.User_table.Where(a => a.UserName == HttpContext.User.Identity.Name).FirstOrDefault(); //get user
+            jawn.UserID = user.UserID;
+            db.Address_table.Add(jawn);
+            db.SaveChanges();
+
+            return View("Index");
+        }
+
+        public ActionResult ConfirmOrder(Address_table jawn)
+        {
+            var user = db.User_table.Where(a => a.UserName == HttpContext.User.Identity.Name).FirstOrDefault(); //get user
+            jawn.UserID = user.UserID;
+            return View("Index");
+        }
+
     }
 }
